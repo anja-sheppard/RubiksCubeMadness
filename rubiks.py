dafd6343b366e1bec2b41250c5e5860a1d85dfbe
amstrudy <amstrudy@gmail.com>
2017-02-04 21:24:33 -0500

    generates cube, no working L code

--- rubiks.py
+++ rubiks.py
@@ -4,7 +4,87 @@ class Cube(object):
     def __init__(self):
         self.cube = [[[[] for k in range(3)] for j in range(3)] for i in range(3)]
     def printy(self):
-        return pprint.pprint(self.cube)
+
+        #top
+        print("\t\t", end = "\t ")
+        for i in range(len(self.cube[0])):
+            print(self.cube[2][0][i].sides["top"], end = "\t")
+        print("")
+        print("\t\t", end = "\t ")
+        for i in range(len(self.cube[0])):
+            print(self.cube[1][0][i].sides["top"], end = "\t")
+        print("")
+        print("\t\t", end = "\t ")
+        for i in range(len(self.cube[0])):
+            print(self.cube[0][0][i].sides["top"], end = "\t")
+        print("\n")
+
+        # left
+        for k in range(len(self.cube[0])):
+            print(self.cube[2][k][0].sides["left"], end = "\t")
+        print(" ", end = "")
+        #front
+        for k in range(len(self.cube[0])):
+            print(self.cube[0][0][k].sides["front"], end = "\t")
+        print(" ", end = "")
+        #right
+        for k in range(len(self.cube[0])):
+            print(self.cube[k][0][2].sides["right"], end = "\t")
+        print(" ", end = "")
+        #back
+        for k in range(len(self.cube[0])):
+            print(self.cube[2][0][2-k].sides["back"], end = "\t")
+        print("")
+        #left
+        for k in range(len(self.cube[0])):
+            print(self.cube[1][k][0].sides["left"], end = "\t")
+        print(" ", end = "")
+        #front
+        for k in range(len(self.cube[0])):
+            print(self.cube[0][1][k].sides["front"], end = "\t")
+        print(" ", end = "")
+        #right
+        for k in range(len(self.cube[0])):
+            print(self.cube[k][1][2].sides["right"], end = "\t")
+        print(" ", end = "")
+        #back
+        for k in range(len(self.cube[0])):
+            print(self.cube[2][1][2-k].sides["back"], end = "\t")
+        print("")
+        #left
+        for k in range(len(self.cube[0])):
+            print(self.cube[0][k][0].sides["left"], end = "\t")
+        print(" ", end = "")
+        #front
+        for k in range(len(self.cube[0])):
+            print(self.cube[0][2][k].sides["front"], end = "\t")
+        print(" ", end = "")
+        #right
+        for k in range(len(self.cube[0])):
+            print(self.cube[k][2][2].sides["right"], end = "\t")
+        print(" ", end = "")
+        #back
+        for k in range(len(self.cube[0])):
+            print(self.cube[2][2][2-k].sides["back"], end = "\t")
+        print("\n")
+
+
+        #bottom
+        print("\t\t", end = "\t ")
+        for i in range(len(self.cube[0])):
+            print(self.cube[0][2][i].sides["bottom"], end = "\t")
+        print("")
+        print("\t\t", end = "\t ")
+        for i in range(len(self.cube[0])):
+            print(self.cube[1][2][i].sides["bottom"], end = "\t")
+        print("")
+        print("\t\t", end = "\t ")
+        for i in range(len(self.cube[0])):
+            print(self.cube[2][2][i].sides["bottom"], end = "\t")
+        print("")
+
+
+        #return pprint.pprint(self.cube)
     def generate(self):
         for i in range(3):
             for j in range(3):
@@ -22,16 +102,21 @@ class Cube(object):
             self.cube[0][i][2].append("right")
             self.cube[1][i][2].append("right")
             self.cube[2][i][2].append("right")
+        for i in range(len(self.cube)):
+            for j in range(len(self.cube[0])):
+                for k in range(len(self.cube[0][0])):
+                    self.cube[i][j][k] = Pieces(self.cube[i][j][k])
+    def L(self):
+        
+        self.printy()
 
 class Pieces(object):
-    def __init__(self, numSides):
-        self.numSides = numSides
+    def __init__(self, listy):
         keys = ["front", "left", "back", "right", "top", "bottom"]
         self.sides = {key: None for key in keys}
-        self.sideList = []
-    def assignColors(self, listy):
         for i in range(len(listy)):
             self.sides[listy[i]] = Cube.colors[listy[i]]
+    #TODO: make str and repr hooks
 
 class Squares(object):
     def __init__(self, color):
@@ -40,3 +125,15 @@ class Squares(object):
         return self.color
     def __repr(self):
         return "'" + str(self) + "'"
+
+
+def play(c):
+    i = input("What's your play? ")
+    if i == "L" or "l":
+        c.L()
+
+
+c = Cube()
+c.generate()
+c.printy
+play(c)

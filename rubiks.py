c236c6e968809efbaae30a475be964d4bc736b5e
amstrudy <amstrudy@gmail.com>
2017-02-06 14:30:34 -0500

    finished version

--- rubiks.py
+++ rubiks.py
@@ -1,87 +1,91 @@
 import pprint
 import copy
+import random
+
 class Cube(object):
-    colors = {"top": "red", "back": "green", "front": "blue", "right": "white", "bottom": "orange", "left": "yellow"}
+    colors = {"top": "  R  ", "back": "  G  ", "front": "  B  ", "right": "  W  ", "bottom": "  P  ", "left": "  Y  "}
     def __init__(self):
         self.cube = [[[[] for k in range(3)] for j in range(3)] for i in range(3)]
     def printy(self):
 
+        colorCodes = {"  G  ": "\x1b[6;37;42m", "  R  ":"\x1b[6;37;41m", "  Y  ":"\x1b[0;37;43m", "  B  ": "\x1b[6;37;44m", "  P  ": "\x1b[6;37;45m", "  W  ": "\x1b[6;30;47m"}
+
         #top
         print("\t\t", end = "\t ")
         for i in range(len(self.cube[0])):
-            print(self.cube[2][0][i].sides["top"], end = "\t")
+            print(colorCodes[self.cube[2][0][i].sides["top"]] + self.cube[2][0][i].sides["top"] + "\x1b[0m", end = "\t")
         print("")
         print("\t\t", end = "\t ")
         for i in range(len(self.cube[0])):
-            print(self.cube[1][0][i].sides["top"], end = "\t")
+            print(colorCodes[self.cube[1][0][i].sides["top"]] + self.cube[1][0][i].sides["top"] + "\x1b[0m", end = "\t")
         print("")
         print("\t\t", end = "\t ")
         for i in range(len(self.cube[0])):
-            print(self.cube[0][0][i].sides["top"], end = "\t")
+            print(colorCodes[self.cube[0][0][i].sides["top"]] + self.cube[0][0][i].sides["top"] + "\x1b[0m", end = "\t")
         print("\n")
 
         #left
         for k in range(len(self.cube[0])):
-            print(self.cube[2-k][0][0].sides["left"], end = "\t")
+            print(colorCodes[self.cube[2-k][0][0].sides["left"]] + self.cube[2-k][0][0].sides["left"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #front
         for k in range(len(self.cube[0])):
-            print(self.cube[0][0][k].sides["front"], end = "\t")
+            print(colorCodes[self.cube[0][0][k].sides["front"]] + self.cube[0][0][k].sides["front"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #right
         for k in range(len(self.cube[0])):
-            print(self.cube[k][0][2].sides["right"], end = "\t")
+            print(colorCodes[self.cube[k][0][2].sides["right"]] + self.cube[k][0][2].sides["right"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #back
         for k in range(len(self.cube[0])):
-            print(self.cube[2][0][2-k].sides["back"], end = "\t")
+            print(colorCodes[self.cube[2][0][2-k].sides["back"]] + self.cube[2][0][2-k].sides["back"] + "\x1b[0m", end = "\t")
         print("")
         #left
         for k in range(len(self.cube[0])):
-            print(self.cube[2-k][1][0].sides["left"], end = "\t")
+            print(colorCodes[self.cube[2-k][1][0].sides["left"]] + self.cube[2-k][1][0].sides["left"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #front
         for k in range(len(self.cube[0])):
-            print(self.cube[0][1][k].sides["front"], end = "\t")
+            print(colorCodes[self.cube[0][1][k].sides["front"]] + self.cube[0][1][k].sides["front"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #right
         for k in range(len(self.cube[0])):
-            print(self.cube[k][1][2].sides["right"], end = "\t")
+            print(colorCodes[self.cube[k][1][2].sides["right"]] + self.cube[k][1][2].sides["right"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #back
         for k in range(len(self.cube[0])):
-            print(self.cube[2][1][2-k].sides["back"], end = "\t")
+            print(colorCodes[self.cube[2][1][2-k].sides["back"]] + self.cube[2][1][2-k].sides["back"] + "\x1b[0m", end = "\t")
         print("")
         #left
         for k in range(len(self.cube[0])):
-            print(self.cube[2-k][2][0].sides["left"], end = "\t")
+            print(colorCodes[self.cube[2-k][2][0].sides["left"]] + self.cube[2-k][2][0].sides["left"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #front
         for k in range(len(self.cube[0])):
-            print(self.cube[0][2][k].sides["front"], end = "\t")
+            print(colorCodes[self.cube[0][2][k].sides["front"]] + self.cube[0][2][k].sides["front"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #right
         for k in range(len(self.cube[0])):
-            print(self.cube[k][2][2].sides["right"], end = "\t")
+            print(colorCodes[self.cube[k][2][2].sides["right"]] + self.cube[k][2][2].sides["right"] + "\x1b[0m", end = "\t")
         print(" ", end = "")
         #back
         for k in range(len(self.cube[0])):
-            print(self.cube[2][2][2-k].sides["back"], end = "\t")
+            print(colorCodes[self.cube[2][2][2-k].sides["back"]] + self.cube[2][2][2-k].sides["back"] + "\x1b[0m", end = "\t")
         print("\n")
 
 
         #bottom
         print("\t\t", end = "\t ")
         for i in range(len(self.cube[0])):
-            print(self.cube[0][2][i].sides["bottom"], end = "\t")
+            print(colorCodes[self.cube[0][2][i].sides["bottom"]] + self.cube[0][2][i].sides["bottom"] + "\x1b[0m", end = "\t")
         print("")
         print("\t\t", end = "\t ")
         for i in range(len(self.cube[0])):
-            print(self.cube[1][2][i].sides["bottom"], end = "\t")
+            print(colorCodes[self.cube[0][2][i].sides["bottom"]] + self.cube[0][2][i].sides["bottom"] + "\x1b[0m", end = "\t")
         print("")
         print("\t\t", end = "\t ")
         for i in range(len(self.cube[0])):
-            print(self.cube[2][2][i].sides["bottom"], end = "\t")
+            print(colorCodes[self.cube[2][2][i].sides["bottom"]] + self.cube[2][2][i].sides["bottom"] + "\x1b[0m", end = "\t")
         print("")
 
     def generate(self):
@@ -105,6 +109,20 @@ class Cube(object):
             for j in range(len(self.cube[0])):
                 for k in range(len(self.cube[0][0])):
                     self.cube[i][j][k] = Pieces(self.cube[i][j][k])
+
+    def isSolved(self, s):
+        for i in range(len(self.cube)):
+            for j in range(len(self.cube[0])):
+                for k in range(len(self.cube[0][0])):
+                    if(self.cube[i][j][k].sides["top"] != s.cube[i][j][k].sides["top"]): return False
+                    elif(self.cube[i][j][k].sides["bottom"] != s.cube[i][j][k].sides["bottom"]): return False
+                    elif(self.cube[i][j][k].sides["right"] != s.cube[i][j][k].sides["right"]): return False
+                    elif(self.cube[i][j][k].sides["left"] != s.cube[i][j][k].sides["left"]): return False
+                    elif(self.cube[i][j][k].sides["front"] != s.cube[i][j][k].sides["front"]): return False
+                    elif(self.cube[i][j][k].sides["back"] != s.cube[i][j][k].sides["back"]): return False
+        return True
+
+
     def L(self):
         temptopList = []
         tempfrontList = []
@@ -144,7 +162,7 @@ class Cube(object):
             self.cube[2-i][0][0].sides["left"] = tempbackList[i].sides["left"]
 
         return self
-        
+
 
     def R(self):
         temptopList = []
@@ -195,15 +213,15 @@ class Cube(object):
             tempfront = Pieces(list(self.cube[0][0][i].sides.keys()))
             tempfront.sides = copyDict(self.cube[0][0][i].sides)
             tempfrontList.append(tempfront)
-            
+
             templeft = Pieces(list(self.cube[i][0][0].sides.keys()))
             templeft.sides = copyDict(self.cube[i][0][0].sides)
             templeftList.append(templeft)
-            
+
             tempright = Pieces(list(self.cube[i][0][2].sides.keys()))
             tempright.sides = copyDict(self.cube[i][0][2].sides)
             temprightList.append(tempright)
-            
+
             tempback = Pieces(list(self.cube[2][0][i].sides.keys()))
             tempback.sides = copyDict(self.cube[2][0][i].sides)
             tempbackList.append(tempback)
@@ -257,7 +275,7 @@ class Cube(object):
             self.cube[i][2][2].sides["bottom"] = tempfrontList[i].sides["bottom"]
 
         return self
-    
+
     def F(self):
         tempbottomList = []
         templeftList = []
@@ -316,7 +334,6 @@ class Cube(object):
             tempbottom.sides = copyDict(self.cube[2][2][i].sides)
             tempbottomList.append(tempbottom)
         for i in range(3):
-            print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
             self.cube[2][2-i][2].sides["right"] = tempbottomList[i].sides["bottom"]
             self.cube[2][2-i][2].sides["back"] = tempbottomList[i].sides["back"]
 
@@ -330,7 +347,7 @@ class Cube(object):
             self.cube[2][2][2-i].sides["back"] = templeftList[i].sides["back"]
 
         return self
-    
+
 class Pieces(object):
     def __init__(self, listy):
         keys = ["front", "left", "back", "right", "top", "bottom"]
@@ -382,9 +399,11 @@ def play(c):
             c = c.B()
     elif i == "Q" or i == "q":
         return 1
+    else:
+        print("Please enter a valid command.")
     c.printy()
     return 0
-        
+
 
 def copyDict(dict1):
     dict2 = {}
@@ -392,11 +411,24 @@ def copyDict(dict1):
         dict2[i] = dict1[i]
     return dict2
 
+def scramble(c):
+    random.seed()
+    moves = [c.R, c.B, c.F, c.U, c.D, c.L]
+    for i in range(80):
+        moves[random.randint(0, 5)]()
 
+
+print("\n" + '\x1b[6;30;42m' + "WELCOME TO RUBIKS.CUBE ! ! !" + '\x1b[0m')
+print("\n\nUse your keyboard to move the pieces (\'B\', \'U\', etc.)")
+print("\nNOTE: For pretty colors, a Unix-based operating system is required.\n\n")
 c = Cube()
+s = Cube()
 c.generate()
+s.generate()
+scramble(c)
 c.printy()
 solved = False
 while solved == False:
-    solved = play(c)
-    
+    play(c)
+    if c.isSolved(s):
+        print("solved!!!!")
